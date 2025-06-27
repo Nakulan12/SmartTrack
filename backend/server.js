c133f994222638a378591a4ce0c9f2ebdae6e22f
Nakulan R <nakulanr307@gmail.com>
2025-06-27 20:48:14 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import { Alert } from './models/alert.js';
-import { initializeApp, getApps } from 'firebase-admin/app';
-import { getAuth as adminAuth } from 'firebase-admin/auth';
