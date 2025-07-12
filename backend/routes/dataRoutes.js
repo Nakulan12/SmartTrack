4413436d5d826270467bd2a8d0e1143a425697c4
Nakulan R <nakulanr307@gmail.com>
2025-07-12 23:17:20 +0530

    Update dataRoutes.js

--- backend/routes/dataRoutes.js
+++ backend/routes/dataRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { addData, getAllData } = require('../controllers/dataController');
+const auth = require('../middleware/authMiddleware');
+
+router.post('/', auth, addData);
+router.get('/', auth, getAllData);
+
+module.exports = router;
+
+
 
